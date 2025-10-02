2abe674315c07708d1cf3591b34715ebe6f3460e
Emanuele Pietro Cometti <133690625+EmanuelePietroCometti@users.noreply.github.com>
2025-10-02 10:50:32 +0200

    v1.0.8

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,18 +1,1 @@
-pub mod fs;  // qui vive la logica
-
-use fs::MyFS;
-use polyfuse::Session;
-use anyhow::Result;
-use tokio::signal;
-
-pub async fn run(mountpoint: &str) -> Result<()> {
-    let mut session = Session::mount(MyFS, mountpoint, &[]).await?;
-    tokio::select! {
-        res = session.run() => res?,
-        _ = signal::ctrl_c() => {
-            println!("Unmounting...");
-        }
-    }
-    Ok(())
-}
 
